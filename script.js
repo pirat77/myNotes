1bdda7ea232f259b14c188ca456122e709dad0f2
Jasio <mcyanek@gmail.com>
2020-07-18 18:33:08 +0200

    implements interactive model, removes uneccessary code

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 document.getElementById("button").addEventListener("click", function(event){
     event.preventDefault();
-    let form = document.getElementById('noteForm');
-    let formData = new FormData(form);
-    let JSONdata = JSON.stringify(formData);
+    let note = document.getElementById('content').value;
+    let JSONdata = JSON.stringify(note);
     localStorage.setItem((localStorage.length+1).toString(), JSONdata);
+    readItems();
 });
 
-function readItem() {
+document.getElementById("clear").addEventListener("click", function(event){
+    event.preventDefault();
     let items = [];
     for (let i = 1; i <= localStorage.length; i++) {
-        items.push("<li id='" + i.toString() + "'>" + JSON.parse(localStorage.getItem(i.toString())) + "</li>" );
+        items.push(JSON.parse(localStorage.getItem(i.toString())));
     }
-    $("<ul/>", {
-        "class": "notes",
-        html: items.join("")
-    }).appendTo("body");
+    localStorage.clear();
+    items.forEach(item => {
+        if (item !== 'solved') {
+            let JSONdata = JSON.stringify(item);
+            localStorage.setItem((localStorage.length+1).toString(), JSONdata);
+        }
+    })
+    readItems();
+});
+
+function readItems() {
+    let items = [];
+    for (let i = 1; i <= localStorage.length; i++) {
+        items.push("<li class=\"note\" id='" + i.toString() + "'>" + JSON.parse(localStorage.getItem(i.toString())) + "</li>");
+    }
+    document.getElementById("notesList").innerHTML = items.join("");
+    allowRemoval();
 }
 
-readItem();
+function allowRemoval(){
+        document.querySelectorAll('.note').forEach(item => {
+            item.addEventListener('mouseover', function(event){
+                item.style.color = 'red';
+            })
+            item.addEventListener('mouseleave', function(event){
+                item.style.color = 'black';
+            })
+            item.addEventListener('click', function(event){
+                localStorage.setItem(item.id, JSON.stringify("solved"));
+                readItems();
+            })
+        })
+}
+
+readItems();
